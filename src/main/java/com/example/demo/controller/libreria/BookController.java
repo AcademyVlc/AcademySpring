42a3ce0885cf40ad069236ac6d0d7f9e4358e74c
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-05-28 17:42:25 +0200

    Esercitazione Ristorante: -ORM completata -Category Controller completato

--- src/main/java/com/example/demo/controller/libreria/BookController.java
+++ src/main/java/com/example/demo/controller/libreria/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
-public class BookRestController {
+public class BookController {
 
     private final BookService bookService;
 
---
+++ src/main/java/com/example/demo/dto/ristorante/CategoryRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.ristorante;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryRequestDTO {
+
+    private Integer id;
+    private String name;
+}

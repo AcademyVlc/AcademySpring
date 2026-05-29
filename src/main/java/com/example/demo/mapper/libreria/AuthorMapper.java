bf67a168f2fd2ed73eb7f2f522d477ca6bf52376
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-05-29 12:33:24 +0200

    AuthorMapper creation

--- src/main/java/com/example/demo/mapper/libreria/AuthorMapper.java
+++ src/main/java/com/example/demo/mapper/libreria/AuthorMapper.java
@@ -1,6 +1,8 @@
 package com.example.demo.mapper.libreria;
 
 import com.example.demo.dto.libreria.AuthorDTO;
+import com.example.demo.dto.libreria.requestdto.AuthorRequestDTO;
+import com.example.demo.dto.libreria.responsedto.AuthorResponseDTO;
 import com.example.demo.entity.libreria.Author;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,17 +12,21 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
 
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "firstname", source = "firstname")
-    @Mapping(target = "lastname", source = "lastname")
-    @Mapping(target = "birthdate", source = "birthdate")
-    @Mapping(target = "eliminationDate", source = "eliminationDate")
-    AuthorDTO entityToDTO(Author author);
+//    @Mapping(target = "id", source = "id")
+//    @Mapping(target = "firstname", source = "firstname")
+//    @Mapping(target = "lastname", source = "lastname")
+//    @Mapping(target = "birthdate", source = "birthdate")
+//    @Mapping(target = "eliminationDate", source = "eliminationDate")
+//    AuthorDTO entityToDTO(Author author);
+//
+//    @Mapping(target = "books", ignore = true)
+//    Author dtoToEntity(AuthorDTO authorDTO);
+//    List<AuthorDTO> entityToDTO(List<Author> authors);
+//    List<Author> dtoToEntity(List<AuthorDTO> authorsDTO);
 
-    @Mapping(target = "books", ignore = true)
-    Author dtoToEntity(AuthorDTO authorDTO);
+    @Mapping(target = , source = )
+    AuthorResponseDTO entityToDTO(Author author);
 
-    List<AuthorDTO> entityToDTO(List<Author> authors);
-
-    List<Author> dtoToEntity(List<AuthorDTO> authorsDTO);
+    @Mapping(target = , source = )
+    Author dtoToEntity(AuthorRequestDTO authorRequestDTO);
 }

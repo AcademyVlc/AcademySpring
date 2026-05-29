21bcaa7b2a18e40d7a843a22ee63185b330c7895
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-05-29 09:52:10 +0200

    Restaurant refactor

--- src/main/java/com/example/demo/repository/libreria/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository.libreria;
-
-import com.example.demo.entity.libreria.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}

79dfc9f1a441243787edec59ab09b7c2774da6ad
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-05-29 10:10:48 +0200

    Main refactor

--- src/main/java/com/example/demo/repository/libreria/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository.libreria;
-
-import com.example.demo.entity.libreria.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Integer> {
-    // Metodi CRUD già implementati da JPA
-}

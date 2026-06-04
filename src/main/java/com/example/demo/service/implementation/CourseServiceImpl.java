83d03ebdd223176f571de9bd6fc86bf40f28602c
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-06-04 17:38:39 +0200

    CourseServiceImpl

--- src/main/java/com/example/demo/service/implementation/CourseServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/CourseServiceImpl.java
@@ -2,16 +2,27 @@ package com.example.demo.service.implementation;
 
 import com.example.demo.dto.request.CourseRequestDTO;
 import com.example.demo.dto.response.CourseResponseDTO;
+import com.example.demo.entity.palestra.Course;
+import com.example.demo.mapper.palestra.CourseMapper;
+import com.example.demo.repository.palestra.CourseRepository;
 import com.example.demo.service.abstraction.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
+@RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
+
     @Override
     public List<CourseResponseDTO> findAll() {
-        return List.of();
+        List<Course> courses = courseRepository.findAll();
+        return courseMapper.entityToResponseDTO(courses);
     }
 
     @Override

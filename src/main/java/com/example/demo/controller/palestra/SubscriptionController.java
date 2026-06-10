f73756b93efad8cfca31b433d796dc35bbfc54e7
stefano-pirastru <stefano.pirastru2001@gmail.com>
2026-06-10 09:26:19 +0200

    Completed SubscriptionController

--- src/main/java/com/example/demo/controller/palestra/SubscriptionController.java
+++ src/main/java/com/example/demo/controller/palestra/SubscriptionController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller.palestra;
 
+import com.example.demo.dto.request.SubscriptionRequestDTO;
 import com.example.demo.dto.response.RevenueDTO;
+import com.example.demo.dto.response.SubscriptionResponseDTO;
 import com.example.demo.service.abstraction.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subscriptions")
@@ -15,7 +17,38 @@ public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
 
-    // Calcolare incasso totale da abbonamenti attivi 
+    @GetMapping
+    public ResponseEntity<List<SubscriptionResponseDTO>> findAll() {
+        List<SubscriptionResponseDTO> subscriptions = subscriptionService.findAll();
+        return ResponseEntity.ok(subscriptions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionResponseDTO> findById(@PathVariable Integer id) {
+        SubscriptionResponseDTO subscription = subscriptionService.findById(id);
+        subscription.setSuccess(true);
+        return ResponseEntity.ok(subscription);
+    }
+
+    @PostMapping
+    public ResponseEntity<SubscriptionResponseDTO> save(@RequestBody SubscriptionRequestDTO subscriptionRequestDTO) {
+        SubscriptionResponseDTO savedSubscription = subscriptionService.save(subscriptionRequestDTO);
+        return ResponseEntity.ok(savedSubscription);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SubscriptionResponseDTO> update(@PathVariable Integer id, @RequestBody SubscriptionRequestDTO subscriptionRequestDTO) {
+        SubscriptionResponseDTO updatedSubscription = subscriptionService.update(id, subscriptionRequestDTO);
+        return ResponseEntity.ok(updatedSubscription);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletedById(@PathVariable Integer id) {
+        subscriptionService.deletedById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Calcolare incasso totale da abbonamenti attivi
     @GetMapping("/active-revenue")
     public ResponseEntity<RevenueDTO> calculateActiveSubscriptionsRevenue() {
         RevenueDTO revenueDTO = subscriptionService.calculateActiveSubscriptionsRevenue();
